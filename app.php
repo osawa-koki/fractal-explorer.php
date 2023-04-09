f2b7ec7a9b3cd4061f19398bc20e3eb393010ade
osawa-koki <github@osawa.dev>
2023-04-09 18:11:28 +0900

    出力部分。

--- app.php
+++ app.php
@@ -2,6 +2,8 @@
 
 require_once("./drawer/mandelbrot.php");
 
+$items = [];
+
 $json_string = file_get_contents("./config.json");
 $config = json_decode($json_string, true);
 
@@ -14,6 +16,9 @@ $mandelbrot_config = $config["mandelbrot"];
 $mandelbrot_config["width"] = $width;
 $mandelbrot_config["height"] = $height;
 $mandelbrot_config["output"] = $output_dir . DIRECTORY_SEPARATOR . $mandelbrot_config["output_file"];
+array_push($items, $mandelbrot_config["output_file"]);
 mandelbrot_drawer($mandelbrot_config);
+
+file_put_contents($output_dir . DIRECTORY_SEPARATOR . "items.txt", join("\n", $items));
 
 ?>
